dca96a7fa2d10fbebf7a788615c2e1d6bc611111
bealecs <clifcodes@gmail.com>
2023-02-06 15:39:10 -0600

    added project that I have been working on

--- src/ProjectsCarousel.js
+++ src/ProjectsCarousel.js
@@ -14,11 +14,11 @@ export default class ProjectSlider extends Component {
     };
     
     const calculator = "https://clipartix.com/wp-content/uploads/2018/03/easy-clipart-2018-4.png";
-    const ball = require("./8ball.png");
     const food = "https://clipartix.com/wp-content/uploads/2018/03/burger-clipart-2018-15.png";
     const xo = require("./xo.png");
+    const ball = "https://pipswithpaul.vercel.app"
     const calcUrl = 'https://bealecs.github.io/portfolio/Game%20Projects/calculator.html';
-    const ballUrl = 'https://bealecs.github.io/portfolio/Game%20Projects/8Ball.html';
+    const ballUrl = 'https://freepngimg.com/thumb/stock_market/25581-7-stock-market-graph-up-transparent-image-thumb.png';
     const xoUrl = 'https://bealecs.github.io/portfolio/Game%20Projects/xo.html';
     const mealUrl = "https://clifscatalog.netlify.app";
 
@@ -27,6 +27,18 @@ export default class ProjectSlider extends Component {
         <div className="slider-container">
         <h2> My Projects</h2>
         <Slider {...settings}>
+        <div className="proj">
+            <div className="proj-inline">
+              <img src={ballUrl} alt="stock market clipart" style={{ width: 300, height: 300 }} />
+              <h2>Pips with Paul</h2>
+            </div>
+            <div className="proj-inline2">
+              <p>This is a project that I am currently working on for a friend. He is a mentor for Forex market training, and is launching a subscription based training platform with my help. I am
+                creating this project using NextJs with Firebase for my user authentication and database.
+              </p>
+              <a href={ball} target="_blank" rel="noopener noreferrer"><button className={this.props.className2}>Go</button></a>
+            </div>
+          </div>
           <div className="proj">
             <div className="proj-inline">
               <img src={food} alt="a hamburger" style={{ width: 300, height: 300 }} />
@@ -62,16 +74,7 @@ export default class ProjectSlider extends Component {
               <a href={xoUrl} target="_blank" rel="noopener noreferrer"><button className={this.props.className2}>Go</button></a>
             </div>
           </div>
-          <div className="proj">
-            <div className="proj-inline">
-              <img src={ball} alt="a magic 8-ball" style={{ width: 300, height: 300 }} />
-              <h2>Magic 8 Ball</h2>
-            </div>
-            <div className="proj-inline2">
-              <p>Just like the old magic 8 ball that you remember, equipped with 9 random responses for all of your best questions: ask away! This project was created using HTML/CSS/JavaScript</p>
-              <a href={ballUrl} target="_blank" rel="noopener noreferrer"><button className={this.props.className2}>Go</button></a>
-            </div>
-          </div>
+          
           
           <div>
             <FeedbackForm />
