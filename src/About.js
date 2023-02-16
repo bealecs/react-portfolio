7c434bd3346ccb3edaa424146d55922e1d6dcbca
bealecs <clifcodes@gmail.com>
2023-02-15 21:33:11 -0600

    changes to about section styling

--- src/About.js
+++ src/About.js
@@ -12,28 +12,28 @@ export const About = (props) => {
             <h2>I'm a simple man</h2>
             <div className={props.aboutCards}>
                 <div className={props.cards}>
-                    <img src={code}/>
+                    <img style={{height:"50%"}} src={code}/>
                     <h4>I like to code</h4>
                     <p>I have had a passion for technology since I was young. I began teaching myself some java at around age 13, and took off from there.
                         I can start coding and get lost in my work and spend hours doing it without getting bored. It's like solving a puzzle for me.
                     </p>
                 </div>
                 <div className={props.cards}>
-                    <img src={gaming} style={{aspectRatio:'1.6'}}/>
+                    <img style={{height:"50%"}} src={gaming}/>
                     <h4>I like gaming</h4>
                     <p>I think my love for coding and my love for gaming go hand in hand. I discovered my passion for coding from playing MMORPGs when I was young. To this day, 
                         I am still up to date on a lot of the popular games and enjoy playing in my free time.
                     </p>
                 </div>
                 <div className={props.cards}>
-                    <img src={working}/>
+                    <img style={{height:"50%"}} src={working}/>
                     <h4>I like to work</h4>
                     <p>I have a strong work ethic when doing work that I enjoy. Like I said, I can go hours and hours coding without getting bored. It keeps my brain and fingers busy, which is good
                         for me! Working out problems as they come is what I do best.
                     </p>
                 </div>
                 <div className={props.cards}>
-                    <img src={family}/>
+                    <img style={{height:"50%"}} src={family}/>
                     <h4>I'm a family man</h4>
                     <p>I got married in September of 2022 to my beautiful wife Marisela. Since then, we closed on our first house! Together we have two puppies, Max and Nymeria, and I wouldn't trade it for the world.</p>
                 </div>
