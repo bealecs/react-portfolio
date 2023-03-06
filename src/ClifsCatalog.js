3eddc6f596e863b3612d8b6e6e2d347759c1f613
bealecs <109564941+bealecs@users.noreply.github.com>
2023-03-06 14:46:18 -0600

    changes made to more info for clifs catalog

--- src/ClifsCatalog.js
+++ src/ClifsCatalog.js
@@ -3,13 +3,28 @@ import { AdvancedNavbar } from "./AdvancedNavbar";
 
 export const ClifsCatalog = () => {
 
-    const pwpLogo = require("./banner.webp");
+    const cclogo = require("./banner.webp");
 
     return (
         <>
             <AdvancedNavbar />
-            <div style={{backgroundColor:'#005C7A', height:'93vh'}}>
-                <img src={pwpLogo} style={{borderRadius:'100%', width:'20%', margin:'1%'}} />
+            <div style={{backgroundColor:'#005C7A', height:'93vh', display:'flex',flexwrap:'wrap', flexDirection:'column', alignItems:'center'}}>
+                <img src={cclogo} style={{borderRadius:'50%', width:'300px', height:'300px', margin:"2% auto"}} alt="logo for Clif's Catalog"/>
+                <p style={{lineHeight:'2rem', color:'#CCDBDC', textAlign:"left",height:'100%',resize:'none'}}>I started Clif's Catalog while I was completing a course offered through Codecademy for front-end web development.
+                <br /><br />
+                This course was an introduction to utilizing API's, and I came across 'TheMealDB' API. This site offered a database of around 1,000 different meals from all over the world for their free use of the API.
+                I utilized their API to create a random meal generator for my wife and I. This project was completed using React JS, and was seemingly boring as it was just a generator. Click a button, get a result...
+                <br /> <br /> Naturally, I wanted to dig a little bit deeper. I found other ways to utilize their database, and changed the site from a generator to a full blown cookbook. 
+                <br /> <br />
+                Now, you can create an account, search for individual meals by category, and still generate random meals if you are feeling indecisive. While this site is still undergoing maintenance,
+                eventually you will be able to save different meals to your favorites section for quick access. 
+                <br /> <br />
+                In the future, I plan to incorporate a larger spectrum of meals & meal categories, and maybe even go as far as to filter meals by allergens and more.
+                <br /> <br />
+                <a href="https://github.com/bealecs/pipswithpaul" target='_blank' rel="noopener noreferrer" style={{textDecoration:'underline', cursor:'pointer'}}>Check out the repository</a>
+                 <br /><br />
+                 <a href="https://pipswithpaul.vercel.app/" target='_blank' rel="noopener noreferrer" style={{textDecoration:'underline', cursor:'pointer'}}>Check out the site</a>
+                </p>
             </div>
         </>
     )
--- src/PipsWithPaul.js
+++ src/PipsWithPaul.js
@@ -9,7 +9,7 @@ export const PipsWithPaul = () => {
         <>
             <AdvancedNavbar />
             <div style={{backgroundColor:'#005C7A', height:'100%',minHeight:'90vh', display:'inline-flex', alignItems:'center', flexWrap:"wrap"}}>
-                <img src={pwpLogo} style={{borderRadius:'50%', width:'300px', height:'300px', margin:"1% auto"}} alt="Pips With Paul Logo"/>
+                <img src={pwpLogo} style={{borderRadius:'10%', width:'300px', height:'300px', margin:"1% auto"}} alt="Pips With Paul Logo"/>
                 <p style={{lineHeight:'2rem', color:'#CCDBDC', textAlign:"left",height:'100%',resize:'none'}}>I started Pips With Paul to really test my skillset at that time.
                 I was starting to become familiar with React, but I had recently come across NextJS documentation and started reading. All it took was learning how NextJS handled routing, and I was hooked.
                 I read through a lot of the documentation to get started, and began Pips With Paul using typescript and NextJS. <br /> <br /> Since starting this project, I have hit a couple of road blocks & learning curves; luckily, there is a lot of good documentation for NextJS, and finally, the site is starting to come together - <br>
@@ -17,9 +17,9 @@ export const PipsWithPaul = () => {
                 to set up and configure Google's sign in SDK. Because I like reading so much, I headed over to Stripe's documentation to add their services as well, seeing as this is a 
                 subscription based service. Now, you can click on the courses and find a plan that is right for you. Once you have the plan that is right for you, your video courses that are pre-built by Paul and his team will appear
                 on your account's dashboard!<br /><br /> Happy learning, go make that money <br /> <br />
-                 <a href="https://vetswhocode.io/" target='_blank' rel="noopener noreferrer" style={{textDecoration:'underline', cursor:'pointer'}}>Check out the repository</a>
+                 <a href="https://github.com/bealecs/pipswithpaul" target='_blank' rel="noopener noreferrer" style={{textDecoration:'underline', cursor:'pointer'}}>Check out the repository</a>
                  <br /><br />
-                 <a href="https://vetswhocode.io/" target='_blank' rel="noopener noreferrer" style={{textDecoration:'underline', cursor:'pointer'}}>Check out the site</a>
+                 <a href="https://pipswithpaul.vercel.app/" target='_blank' rel="noopener noreferrer" style={{textDecoration:'underline', cursor:'pointer'}}>Check out the site</a>
                  </p>
 
             </div>
